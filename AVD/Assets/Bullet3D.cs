3508150cd0041087aa9d24f262cdf2ee89919b94
Joshua <joshi.baeck@gmail.com>
2020-02-28 19:29:23 +0100

    FridaysTaskTurret

---
+++ AVD/Assets/Bullet3D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet3D : MonoBehaviour
+{
+    public float speed = 100f;
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+        //rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+        rb.AddForce(transform.position * speed);
+       
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AVD/Assets/Weapon3D.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon3D : MonoBehaviour
+{
+
+    public Transform firePoint1;
+    public Transform firePoint2;
+    public Transform firePoint3;
+    public Transform firePoint4;
+    public GameObject bulletPrefab;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint1.position, firePoint1.rotation);
+        Instantiate(bulletPrefab, firePoint2.position, firePoint2.rotation);
+        Instantiate(bulletPrefab, firePoint3.position, firePoint3.rotation);
+        Instantiate(bulletPrefab, firePoint4.position, firePoint4.rotation);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
